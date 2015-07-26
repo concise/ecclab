2e73639f90be45c57e09ac3630fc4f71a570e857
Jong-Shian Wu <js@crypto.tw>
2015-07-26 15:10:45 +0800

    update

--- asn1.py
+++ asn1.py
@@ -3,65 +3,99 @@ NOT_AN_ASN1_ENCODED = 'not an ASN.1 encoded result'
 BAD_ASN1_OR_TOO_LONG = ('the ASN.1 encoded object has a value longer than '
                         '16 MiB or is corrupted')
 
-def parse_asn1_sequence(stream):
+def asn1_parse_sequence(stream):
     """
-    parse_asn1_sequence parses an octet string as an ASN.1 encoded T-L-V
-    triple just as what parse_asn1_anything does, but the V will be replaced
+    asn1_parse_sequence parses an octet string as an ASN.1 encoded T-L-V
+    triple just as what _asn1_parse_anything does, but the V will be replaced
     with a tuple of bytes representing the ordered sequence in the provided
     ASN.1 SEQUENCE value.
     """
-    raise NotImplementedError # TODO
+    t, l, v = _asn1_parse_anything(stream)
+    if t != 0x30:
+        raise ValueError(NOT_AN_ASN1_ENCODED)
 
-def parse_asn1_integer(stream):
-    """
-    parse_asn1_integer parses an octet string as an ASN.1 encoded T-L-V triple
-    just as what parse_asn1_anything does, but the V will be replaced with an
-    int representing the signed integer value of the provided ASN.1 INTEGER.
-    """
-    raise NotImplementedError # TODO
+    raise NotImplementedError
+    # TODO repeatedly parse the substream `v` until the end
 
-def parse_asn1_anything(tlv):
+def asn1_parse_integer(stream):
+    t, l, v = _asn1_parse_anything(stream)
+    if t != 0x02:
+        raise ValueError(NOT_AN_ASN1_ENCODED)
+    if l == 0:
+        raise ValueError(NOT_AN_ASN1_ENCODED)
+    if l >= 2 and v[0] == 0x00 and v[1] == 0x00:
+        raise ValueError(NOT_AN_ASN1_ENCODED)
+    intval = int.from_bytes(v, byteorder='big', signed=True)
+    return intval
+
+def _asn1_parse_anything(tlv):
+    t, l, v, x = _asn1_parse_tlvx(tlv)
+    if len(x) != 0:
+        raise ValueError(NOT_AN_ASN1_ENCODED)
+    else:
+        assert type(t) is int and 0 <= t <= 255
+        assert type(l) is int and l >= 0
+        assert type(v) is bytes and len(v) == l
+        assert len(tlv) == 1 + _length_of_asn1_length_field(l) + l
+        return t, l, v
+
+def _length_of_asn1_length_field(l):
+    if type(l) is not int:
+        assert False
+    elif 0 <= l < 0x80:
+        return 1
+    elif l < 0x0100:
+        return 2
+    elif l < 0x010000:
+        return 3
+    elif l < 0x01000000:
+        return 4
+    else:
+        assert False
+
+def _asn1_parse_tlvx(tlvx):
     """
-    parse_asn1_anything parses an octet string as an ASN.1 encoded T-L-V
+    _asn1_parse_tlvx parses an octet string as an ASN.1 encoded T-L-V-X
     triple.
 
-    Input data type:    tlv should be a bytes
-    Output data types:  a tuple of three objects T, L, and V,
-                        where T and L is an int and V is a bytes
+    Input data type:    tlvx should be a bytes
+    Output data types:  a tuple of three objects T, L, and V, where both T and
+                        L is an int and both V and X is a bytes
     """
-    if type(tlv) is not bytes:
+    if type(tlvx) is not bytes:
         raise TypeError(NOT_A_PYTHON_BYTES)
-    if len(tlv) < 2:
+    if len(tlvx) < 2:
         raise ValueError(NOT_AN_ASN1_ENCODED)
-    if tlv[1] == 128:
+    if tlvx[1] == 128:
         raise ValueError(NOT_AN_ASN1_ENCODED)
-    t = tlv[0]
-    lv = tlv[1:]
-    if lv[0] <= 127:
-        l, v = parse_asn1_length_value_pair_with_short_length(lv)
+    t = tlvx[0]
+    lvx = tlvx[1:]
+    if lvx[0] <= 127:
+        l, v, x = _asn1_parse_lvx_with_short_l(lvx)
     else:
-        l, v = parse_asn1_length_value_pair_with_long_length(lv)
-    return t, l, v
+        l, v, x = _asn1_parse_lvx_with_long_l(lvx)
+    return t, l, v, x
 
-def parse_asn1_length_value_pair_with_short_length(lv):
-    assert type(lv) is bytes
-    assert lv[0] <= 127
-    l = lv[0]
-    v = lv[1:]
-    if len(v) == l:
-        return l, v
-    else
+def _asn1_parse_lvx_with_short_l(lvx):
+    assert type(lvx) is bytes
+    assert lvx[0] <= 127
+    l = lvx[0]
+    vx = lvx[1:]
+    if len(vx) >= l:
+        v, x = vx[:l], vx[l:]
+        return l, v, x
+    else:
         raise ValueError(NOT_AN_ASN1_ENCODED)
 
-def parse_asn1_length_value_pair_with_long_length(lv):
-    assert type(lv) is bytes
-    assert lv[0] >= 129
-    if lv[0] - 0x80 == 1:
-        return parse_asn1_length_value_pair_with_2_length_octets(lv)
-    elif lv[0] - 0x80 == 2:
-        return parse_asn1_length_value_pair_with_3_length_octets(lv)
-    elif lv[0] - 0x80 == 3:
-        return parse_asn1_length_value_pair_with_4_length_octets(lv)
+def _asn1_parse_lvx_with_long_l(lvx):
+    assert type(lvx) is bytes
+    assert lvx[0] >= 129
+    if lvx[0] - 0x80 == 1:
+        return _asn1_parse_lvx_with_2_l_octets(lvx)
+    elif lvx[0] - 0x80 == 2:
+        return _asn1_parse_lvx_with_3_l_octets(lvx)
+    elif lvx[0] - 0x80 == 3:
+        return _asn1_parse_lvx_with_4_l_octets(lvx)
     else:
         #
         # A shortest valid ASN.1 L-V octet string in this case will be:
@@ -70,37 +104,40 @@ def parse_asn1_length_value_pair_with_long_length(lv):
         # | 0x84 | 0x01 | 0x00 | 0x00 | 0x00 | ??????????????????????? |
         # +------+------+------+------+------+-------------------------+
         #                                         0x01000000 octets
-        if len(lv) < 5 + 0x01000000:
+        if len(lvx) < 5 + 0x01000000:
             raise ValueError(NOT_AN_ASN1_ENCODED)
         else:
             raise ValueError(BAD_ASN1_OR_TOO_LONG)
 
-def parse_asn1_length_value_pair_with_2_length_octets(lv):
-    assert type(lv) is bytes
-    assert lv[0] - 0x80 = 1
-    l = int.from_bytes(lv[1:2], byteorder='big')
-    v = lv[2:]
-    if 0x80 <= l <= 0xff and len(v) == l:
-        return l, v
+def _asn1_parse_lvx_with_2_l_octets(lvx):
+    assert type(lvx) is bytes
+    assert lvx[0] - 0x80 == 1
+    l = int.from_bytes(lvx[1:2], byteorder='big')
+    vx = lvx[2:]
+    if 0x80 <= l <= 0xff and len(vx) >= l:
+        v, x = vx[:l], vx[l:]
+        return l, v, x
     else:
         raise ValueError(NOT_AN_ASN1_ENCODED)
 
-def parse_asn1_length_value_pair_with_3_length_octets(lv):
-    assert type(lv) is bytes
-    assert lv[0] - 0x80 = 2
-    l = int.from_bytes(lv[1:3], byteorder='big')
-    v = lv[3:]
-    if 0x0100 <= l <= 0xffff and len(v) == l:
-        return l, v
+def _asn1_parse_lvx_with_3_l_octets(lvx):
+    assert type(lvx) is bytes
+    assert lvx[0] - 0x80 == 2
+    l = int.from_bytes(lvx[1:3], byteorder='big')
+    vx = lvx[3:]
+    if 0x0100 <= l <= 0xffff and len(vx) >= l:
+        v, x = vx[:l], vx[l:]
+        return l, v, x
     else:
         raise ValueError(NOT_AN_ASN1_ENCODED)
 
-def parse_asn1_length_value_pair_with_4_length_octets(lv):
-    assert type(lv) is bytes
-    assert lv[0] - 0x80 = 3
-    l = int.from_bytes(lv[1:4], byteorder='big')
-    v = lv[4:]
-    if 0x010000 <= l <= 0xffffff and len(v) == l:
-        return l, v
+def _asn1_parse_lvx_with_4_l_octets(lvx):
+    assert type(lvx) is bytes
+    assert lvx[0] - 0x80 == 3
+    l = int.from_bytes(lvx[1:4], byteorder='big')
+    vx = lvx[4:]
+    if 0x010000 <= l <= 0xffffff and len(vx) == l:
+        v, x = vx[:l], vx[l:]
+        return l, v, x
     else:
         raise ValueError(NOT_AN_ASN1_ENCODED)
