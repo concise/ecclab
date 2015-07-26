388e345170ed106d5c1e03732143158ed9f3803e
Jong-Shian Wu <js@crypto.tw>
2015-07-26 22:00:23 +0800

    update

--- p256.py
+++ p256.py
@@ -33,3 +33,66 @@ xG = 0x6b17d1f2e12c4247f8bce6e563a440f277037d812deb33a0f4a13945d898c296
 yG = 0x4fe342e2fe1a7f9b8ee7eb4a7c0f9e162bce33576b315ececbb6406837bf51f5
 q  = 0xffffffff00000000ffffffffffffffffbce6faada7179e84f3b9cac2fc632551
 h  = 1
+
+def GFp(m):
+    if type(m) is not int:
+        raise P256TypeError
+    return m % p
+
+def GFp_from_bytes(stream):
+    if type(stream) is not bytes:
+        raise P256TypeError
+    if len(stream) != 32:
+        raise P256EncodedInputValueError
+    m = int.from_bytes(stream, byteorder='big', signed=False)
+    if 0 <= m <= p - 1:
+        return m
+    else:
+        raise P256EncodedInputValueError
+
+def GFp_to_bytes(m):
+    if type(m) is not int:
+        raise P256TypeError
+    m = m % p
+    return m.to_bytes(length=32, byteorder='big', signed=False)
+
+def GFp_eq(m, n):
+    if type(m) is not int or type(n) is not int:
+        raise P256TypeError
+    return (m - n) % p == 0
+
+def GFp_neg(m):
+    if type(m) is not int:
+        raise P256TypeError
+    return -m % p
+
+def GFp_add(m, n):
+    if type(m) is not int or type(n) is not int:
+        raise P256TypeError
+    return (m + n) % p
+
+def GFp_inv(m):
+    if type(m) is not int:
+        raise P256TypeError
+    return pow(m, p - 2, p)
+
+def GFp_mul(m, n):
+    if type(m) is not int or type(n) is not int:
+        raise P256TypeError
+    return (m * n) % p
+
+def GFp_sqrt(n, parity):
+    if type(n) is not int or type(parity) is not int:
+        raise P256TypeError
+    m = pow(n, (p + 1) // 4, p)
+    if (m * m - n) % p != 0:
+        raise P256SquareRootNotExistsValueError
+    if GFp_parity_of(m) == parity & 1:
+        return m
+    else:
+        return GFp_neg(m)
+
+def GFp_parity_of(m):
+    if type(n) is not int:
+        raise P256TypeError
+    return (m % p) & 1
