e9dc85b12d77893b687975178ee13af4ebb0d966
Jong-Shian Wu <jongshian.wu@gmail.com>
2015-08-06 17:42:48 +0800

    embed is_prime() into the fast_secp256r1.py script

--- fast_secp256r1.py
+++ fast_secp256r1.py
@@ -40,7 +40,6 @@ xZ = 0
 yZ = 0
 
 def _SELF_CHECK():
-    from primality_test import is_prime
     assert is_prime(p)
     assert is_prime(q)
     assert p & 3 == 3
@@ -231,6 +230,58 @@ def mul(xP, yP, k):
         return xP, p - yP
     else:
         return MontgomeryLadderCoZ(xP, yP, k)
+
+def is_prime(n):
+
+    # generates min(k, n - 3) integers in the range [2, n - 2]
+    def random_integers(n, k):
+        import random
+        if k >= n - 3:
+            yield from range(2, n - 1)
+        elif n <= 0xffff:
+            yield from random.sample(range(2, n - 1), k)
+        else:
+            for _ in range(k):
+                yield random.randint(2, n - 2)
+
+    # test the primality of n using a
+    def rabin_miller_test(n, r, s, a):
+        x = pow(a, s, n)
+        if x == 1 or x == n - 1:
+            return True
+        for j in range(1, r):
+            x = pow(x, 2, n) # x = pow(a, s * (2 ** j), n)
+            if x == 1:
+                return False
+            if x == n - 1:
+                return True
+        return False
+
+    # repeat the test a few times until the required security level is met
+    def probabilistic_primality_test(n, security_level):
+        k = (security_level >> 1) + (security_level & 1)
+        r, s = 0, n - 1
+        while (s & 1) == 0:
+            r, s = (r + 1), (s >> 1)
+        for a in random_integers(n, k):
+            if rabin_miller_test(n, r, s, a):
+                continue
+            else:
+                return False
+        return True
+
+    if type(n) is not int:
+        raise TypeError('is_prime() accepts an integer greater than 1')
+    if n < 2:
+        raise ValueError('is_prime() accepts an integer greater than 1')
+    if n & 1 == 0:
+        return False
+    if n == 2 or n == 3:
+        return True
+
+    # False means n is composite
+    # True  means n is probably prime
+    return probabilistic_primality_test(n, security_level=128)
 
 _SELF_CHECK()
 
